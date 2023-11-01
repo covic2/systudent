dbe8fab172290c6af01a71a2bb12b73701b233e2
covic1 <cesar.villalba.cortes@gmail.com>
2023-10-31 20:54:37 -0600

    modificacion discovery
    se modifica empaquetado discovery

--- systudent-discovery/src/main/java/com/systudent/principal/ServletInitializer.java
+++ systudent-discovery/src/main/java/com/systudent/principal/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.systudent.principal;
+/*package com.systudent.principal;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,3 +11,4 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	}
 
 }
+*/
